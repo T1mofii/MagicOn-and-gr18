c820dbc50fd043a4e6fba849537ca9df0acb3b57
aandrea-alex <151180549+aandrea-alex@users.noreply.github.com>
2024-05-03 17:33:24 +0200

    Add js-function for get current year

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
 //Remove the comment from the next line after adding the mobile menu and header to the project.
 
 // import './js/open-close-mobile-menu';
+
+// Add current year in document
+import './js/current-year';
